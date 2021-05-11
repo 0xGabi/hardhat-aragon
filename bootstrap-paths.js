2711baf9f6de77e45f45124f0a42e92440b122c7
Gabriel <0xgabi@gmail.com>
2021-05-11 20:41:02 -0300

    🌈

--- bootstrap-paths.js
+++
@@ -1,9 +0,0 @@
-import { compilerOptions } from './tsconfig.json'
-import { register } from 'tsconfig-paths'
-import { join } from 'path'
-
-const baseUrl = join(__dirname, './')
-register({
-  baseUrl,
-  paths: compilerOptions.paths
-})
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,91 @@
-import { extendConfig } from 'hardhat/config'
-import '../bootstrap-paths'
-import { configExtender } from './config'
+import { extendConfig, extendEnvironment, task } from 'hardhat/config'
+import { lazyObject } from 'hardhat/plugins'
+import { HardhatConfig, HardhatUserConfig } from 'hardhat/types'
+import path from 'path'
 
-// TODO: Don't use any type below, try to use something like these...
-// import { ResolvedHardhatConfig, HardhatConfig } from 'hardhat/types'
+import { ExampleHardhatRuntimeEnvironmentField } from './ExampleHardhatRuntimeEnvironmentField'
+// This import is needed to let the TypeScript compiler know that it should include your type
+// extensions in your npm package's types file.
+import './type-extensions'
 
-export default function(): void {
-  // Resolve tsconfig-paths at runtime.
-  require('../bootstrap-paths.js')
+export const TASK_PUBLISH = 'publish'
 
-  // Plugin dependencies.
-  // usePlugin('hardhat-etherscan')
-
-  // Task definitions.
-  // Note: buidler sets up the environment by appending tasks to it. This happens when a task() function is called. Therefore, task() must be called on every test run, not only for the first time. However, when node imports a file with require the module body will only be run once on the first time and then use the cached result.
-  /* eslint-disable @typescript-eslint/no-var-requires */
-  const { setupPublishTask } = require('./tasks/publish')
-  setupPublishTask()
-  /* eslint-enable @typescript-eslint/no-var-requires */
-
-  // Environment extensions.
-  // No extensions atm.
-
-  // Default configuration values.
-  extendConfig(configExtender)
+function normalizePath(
+  config: HardhatConfig,
+  userPath: string | undefined,
+  defaultPath: string
+): string {
+  if (userPath === undefined) {
+    userPath = path.join(config.paths.root, defaultPath)
+  } else {
+    if (!path.isAbsolute(userPath)) {
+      userPath = path.normalize(path.join(config.paths.root, userPath))
+    }
+  }
+  return userPath
 }
+
+extendConfig(
+  (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
+    config.paths.dist = normalizePath(config, userConfig.paths?.dist, 'dist')
+  }
+)
+
+extendEnvironment(hre => {
+  // We add a field to the Hardhat Runtime Environment here.
+  // We use lazyObject to avoid initializing things until they are actually
+  // needed.
+  hre.example = lazyObject(() => new ExampleHardhatRuntimeEnvironmentField())
+})
+
+task(TASK_PUBLISH, 'Publish a new app version')
+  .addPositionalParam(
+    'bump',
+    'Type of bump (major, minor or patch) or semantic version',
+    undefined,
+    types.string
+  )
+  .addOptionalParam(
+    'contract',
+    'Contract address previously deployed.',
+    undefined,
+    types.string
+  )
+  .addOptionalParam(
+    'managerAddress',
+    'Owner of the APM repo. Must be provided in the initial release',
+    undefined,
+    types.string
+  )
+  .addOptionalParam(
+    'ipfsApiUrl',
+    'IPFS API URL to connect to an ipfs daemon API server',
+    'http://localhost:5001',
+    types.string
+  )
+  .addFlag(
+    'onlyContent',
+    'Prevents contract compilation, deployment and artifact generation.'
+  )
+  .addFlag('skipValidation', 'Skip validation of artifacts files.')
+  .addFlag('dryRun', 'Output tx data without broadcasting')
+  .setAction(
+    async (
+      params,
+      hre: HardhatRuntimeEnvironment
+    ): Promise<apm.PublishVersionTxData> => {
+      // Do param type verification here and call publishTask with clean params
+      return await publishTask(
+        {
+          bumpOrVersion: params.bump,
+          existingContractAddress: params.contract,
+          managerAddress: params.managerAddress,
+          ipfsApiUrl: params.ipfsApiUrl,
+          onlyContent: params.onlyContent,
+          skipValidation: params.skipValidation,
+          dryRun: params.dryRun
+        },
+        hre
+      )
+    }
+  )
